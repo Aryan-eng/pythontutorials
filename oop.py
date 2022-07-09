b6121fa1822a204d31fb661b42edf56ac155c96e
“Aryan <72766369+Aryan-eng@users.noreply.github.com>
2022-07-09 14:52:00 -0400

    OOP class creation

---
+++ oop.py
@@ -0,0 +1,24 @@
+class player:
+    raise_amount = 1.1
+    contract_years = 2
+    def __init__(self, first, last, pay, team):
+        self.first = first
+        self.last = last
+        self.pay = pay
+        self.team = team
+
+    def extend_contract(self, years):
+        self.contract_years += years
+
+    def full_name(self):
+        return "{} {}".format(self.first, self.last)
+
+    def apply_raise(self):
+        self.pay = (self.pay * self.raise_amount)
+    
+    @classmethod
+    def set_raise_amt(cls, amount):
+        cls.raise_amt = amount
+
+
+    spurs_striker = player("Harry", "Kane", 25000000, "Tottenham Spurs")
